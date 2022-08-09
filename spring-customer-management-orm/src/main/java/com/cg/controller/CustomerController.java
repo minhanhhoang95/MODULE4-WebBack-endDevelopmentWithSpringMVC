54778b713c0380f593c193a36cf9ac107f544367
minhanhhoang95 <hoanganhaminh23071995@gmail.com>
2022-08-09 13:40:49 +0700

    "task"

---
+++ spring-customer-management-orm/src/main/java/com/cg/controller/CustomerController.java
@@ -0,0 +1,66 @@
+package com.cg.controller;
+
+import com.cg.model.Customer;
+import com.cg.service.HibernateCustomerServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/customers")
+public class CustomerController {
+    @Autowired
+    private HibernateCustomerServiceImpl hibernateCustomerService;
+
+    @GetMapping
+    public ModelAndView showCustomerListPage() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("customers/list");
+
+        List<Customer> customerList = hibernateCustomerService.findAll();
+
+        modelAndView.addObject("customers", customerList);
+
+        return modelAndView;
+    }
+    @GetMapping("/{id}")
+    public ModelAndView showCustomerByIdPage(@PathVariable Long id){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("customers/info");
+
+        Customer customer = hibernateCustomerService.findOne(id);
+        modelAndView.addObject("customer",customer);
+
+        return modelAndView;
+    }
+    @GetMapping("/create")
+    public ModelAndView showCreate(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("customers/create");
+
+        Customer customer = new Customer();
+        modelAndView.addObject("customer",customer);
+
+        return modelAndView;
+    }
+    @PostMapping("/{id}")
+    public String doCreate(@PathVariable Long id, @ModelAttribute Customer customer){
+
+        hibernateCustomerService.save(customer);
+
+       return "redirect:/customers";
+    }
+
+    @PostMapping("/update")
+    public String doUpdate(@ModelAttribute Customer customer){
+
+        hibernateCustomerService.save(customer);
+
+        return "redirect:/customers";
+    }
+
+}
