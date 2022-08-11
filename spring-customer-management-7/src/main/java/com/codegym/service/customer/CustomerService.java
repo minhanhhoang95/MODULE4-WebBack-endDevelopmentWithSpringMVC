3aa7923328cfb969d22266287dab7caa8aaeeba6
minhanhhoang95 <hoanganhaminh23071995@gmail.com>
2022-08-11 11:24:43 +0700

    "th bai7"

--- spring-customer-management-7/src/main/java/com/codegym/service/customer/CustomerService.java
+++ spring-customer-management-7/src/main/java/com/codegym/service/customer/CustomerService.java
@@ -4,6 +4,8 @@ import com.codegym.model.Customer;
 import com.codegym.model.Province;
 import com.codegym.repository.ICustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -12,6 +14,15 @@ import java.util.Optional;
 public class CustomerService implements ICustomerService{
     @Autowired
     private ICustomerRepository customerRepository;
+    @Override
+    public Page<Customer> findAll(Pageable pageable) {
+        return customerRepository.findAll(pageable);
+    }
+
+    @Override
+    public Page<Customer> findAllByFirstNameContaining(String firstname, Pageable pageable) {
+        return customerRepository.findAllByFirstNameContaining(firstname, pageable);
+    }
 
     @Override
     public Iterable<Customer> findAll() {
